566017730e5c01f7d71623a027125b674a7ea2b5
Jone Bulande <JoneBulande@gmail.com>
2023-07-20 08:21:38 +0200

    Estrutura Condicional - Exercicio 05 - Troco Verificado

---
+++ 02 Estrutura Condicional/j05TrocoVerificado.java
@@ -0,0 +1,55 @@
+
+// Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia.
+// O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto,
+// e o valor em dinheiro dado pelo cliente. Seu programa deve mostrar o valor do troco a ser devolvido
+// ao cliente. Se o dinheiro dado pelo cliente não for suficiente, mostrar uma mensagem informando o
+// valor restante conforme exemplo
+
+// Exemplo 1
+// Preço unitário do produto: 8.00
+// Quantidade comprada: 2
+// Dinheiro recebido: 20.00
+// TROCO = 4.00 
+
+// Exemplo 2
+// Preço unitário do produto: 30.00
+// Quantidade comprada: 3
+// Dinheiro recebido: 70.00
+// DINHEIRO INSUFICIENTE. FALTAM 20.00 REAIS 
+
+
+import java.util.Scanner;
+
+public class j05TrocoVerificado {
+  public static void main(String[] args) {
+    
+    // 
+    Scanner entrada = new Scanner(System.in);
+    double precoProduto, quantidadeComprada, dinheiroRecebido, troco, valorAPagar;
+
+    
+    // 
+    System.out.print("Preco unitario do produto: ");
+    precoProduto = entrada.nextDouble();
+
+    System.out.print("Quantidade comprada: ");
+    quantidadeComprada = entrada.nextDouble();
+
+    System.out.print("Dinheiro recebido: ");
+    dinheiroRecebido = entrada.nextDouble();
+
+    
+    // 
+    valorAPagar = quantidadeComprada * precoProduto;
+    troco = Math.abs(dinheiroRecebido - valorAPagar);
+    
+    // 
+    if(dinheiroRecebido < valorAPagar) {
+      System.out.printf("%nDINHEIRO INSUFICIENTE. FALTAM %.2f REAIS%n", troco);
+    } else {
+      System.out.printf("%nTROCO = %.2f%n", troco);
+    }
+
+    entrada.close();
+  }
+}
