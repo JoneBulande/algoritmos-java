e34db085da7a6dfe9e88da4157e1d6286611afaa
Jone Bulande <JoneBulande@gmail.com>
2023-07-25 22:54:43 +0200

    03 Estruturas Repetitivas - while - exercicio 05 - validacao de notas

---
+++ 03 Estruturas Repetitivas/01 While/j05validacaoDeNota.java
@@ -0,0 +1,55 @@
+// VALIDAÇÃO DE NOTA
+
+// Faça um programa que leia as notas referentes às duas avaliações de um aluno. Calcule e imprima a
+// média semestral. Faça com que o algoritmo só aceite notas válidas (uma nota válida deve pertencer ao
+// intervalo [0,10]). Cada nota deve ser validada separadamente.
+
+// Exemplo 1:
+// Digite a primeira nota: 3.5
+// Digite a segunda nota: 10.0
+// MEDIA = 6.75
+
+// Exemplo 2:
+// Digite a primeira nota: -3.5
+// Valor invalido! Tente novamente: 3.5
+// Digite a segunda nota: 11.0
+// Valor invalido! Tente novamente: 10.5
+// Valor invalido! Tente novamente: 10.0
+// MEDIA = 6.75
+
+import java.util.Scanner;
+
+public class j05validacaoDeNota {
+  public static void main(String[] args) {
+
+    Scanner entrada = new Scanner(System.in);
+    double nota1, nota2, media;
+
+    System.out.print("Digite a primeira nota: ");
+    nota1 = entrada.nextDouble();
+
+    while (nota1 < 0 || nota1 > 10) {
+
+      System.out.print("Valor invalido! Tente novamente: ");
+      nota1 = entrada.nextDouble();
+
+    }
+
+    System.out.print("Digite a segunda nota: ");
+    nota2 = entrada.nextDouble();
+
+    while (nota2 < 0 || nota2 > 10) {
+
+      System.out.print("Valor invalido! Tente novamente: ");
+
+      nota2 = entrada.nextDouble();
+
+    }
+
+    media = (nota1 + nota2) / 2;
+
+    System.out.printf("MEDIA = %.2f\n", media);
+
+    entrada.close();
+  }
+}
